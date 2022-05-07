520934b9eb3e3aaa70c6a035eaea4c26271870d8
kpodlaski <podlaski@uni.lodz.pl>
2022-05-07 15:54:40 +0200

    koniec zajęć G1 7.05.2022

--- zaoczne/G1/Junity/src/main/java/simple_example/VectorOperations.java
+++ zaoczne/G1/Junity/src/main/java/simple_example/VectorOperations.java
@@ -3,9 +3,9 @@ package simple_example;
 public class VectorOperations {
 
     //Oba wektory a, b tej samej długości, jeśli nie zwracam null
-    double[] addTwoVectors(double[] a, double[] b){
+    public double[] addTwoVectors(double[] a, double[] b) throws Exception {
         if (a.length!= b.length)
-            return null;
+            throw new Exception("Two vectors of differen lenght");
         double[] result = new double[a.length];
         for (int i=0; i< a.length; i++){
             result[i]=a[i]+b[i];
@@ -13,5 +13,39 @@ public class VectorOperations {
         return result;
     }
 
+    public double[] scaleVector(double f, double a[]){
+        double r[] = new double[a.length];
+        for (int i=0; i<r.length; i++){
+            r[i]=f*a[i];
+        }
+        return r;
+    }
 
+    public void scaleVectorChangeOriginal(double f, double a[]){
+        for (int i=0; i<a.length; i++){
+            a[i]=f * a[i];
+        }
+    }
+
+    private double sumOfVectorElements(double a[]){
+        double s = 0;
+        for ( double e: a) {
+            s+=e;
+        }
+        return s;
+    }
+
+    public double[] multiplyTwoVectors(double a[], double b[]){
+        double r[] = new double[a.length];
+        for (int i=0; i<a.length; i++){
+            r[i]=a[i]*b[i];
+        }
+        return r;
+    }
+
+    public double vectorLenght(double a[]){
+        double r[] = multiplyTwoVectors(a,a);
+        double s = sumOfVectorElements(r);
+        return Math.sqrt(s);
+    }
 }
--- zaoczne/G1/Junity/src/test/java/simple_example/VectorOperationsTest.java
+++ zaoczne/G1/Junity/src/test/java/simple_example/VectorOperationsTest.java
@@ -20,14 +20,14 @@ class VectorOperationsTest {
     }
 
     @Test
-    void addTwoVectors() {
+    void addTwoVectors() throws Exception {
         double a[] = new double[] {1.0, 2.0, 4.0};
         double b[] = new double[] {-1.0, -2.0, 4.0};
         double r[];
         double exp[] = new double[] {0,0,8.0};
         r =tobject.addTwoVectors(a,b);
         for(int i=0; i<r.length; i++) {
-            assertEquals(exp[i], r[i], 0.0001, " ba value at index " + i);
+            assertEquals(exp[i], r[i], 0.0001, " bad value at index " + i);
         }
     }
 
@@ -36,7 +36,60 @@ class VectorOperationsTest {
         double a[] = new double[] {1.0, 2.0, 4.0};
         double b[] = new double[] {-1.0, -2.0, 4.0, 1.1};
         double r[];
-        r =tobject.addTwoVectors(a,b);
-        assertNull(r);
+        assertThrows(Exception.class, () ->{tobject.addTwoVectors(a,b);} );
     }
+
+    @Test
+    void scaleVectorMultiplyBy0() throws Exception{
+        double a[] = new double[] {1.0, 2.0, 4.0};
+        double r[];
+        r = tobject.scaleVector(0,a);
+        assertEquals(a.length,r.length,"bad lenght o resulting vector");
+        for(int i=0; i<r.length; i++) {
+            assertEquals(0, r[i], 0.0001, " bad value at index " + i);
+        }
+    }
+
+    @Test
+    void scaleVectorMultiplyBy2() throws Exception{
+        double a[] = new double[] {1.0, 2.0, 4.0};
+        double exp[] = new double[] {2.0, 4.0, 8.0};
+        double r[];
+        r = tobject.scaleVector(2,a);
+        assertEquals(a.length,r.length,"bad lenght o resulting vector");
+        for(int i=0; i<r.length; i++) {
+            assertEquals(exp[i], r[i], 0.0001, " bad value at index " + i);
+        }
+    }
+
+    @Test
+    void scaleVectorMultiplyBy0_v2() throws Exception{
+        double a[] = new double[] {1.0, 2.0, 4.0};
+        double r[];
+        tobject.scaleVectorChangeOriginal(0,a);
+        for(int i=0; i<a.length; i++) {
+            assertEquals(0, a[i], 0.0001, " bad value at index " + i);
+        }
+    }
+
+    @Test
+    void scaleVectorMultiplyBy2_v2() throws Exception{
+        double a[] = new double[] {1.0, 2.0, 4.0};
+        double exp[] = new double[] {2.0, 4.0, 8.0};
+        double b[] = a;
+        tobject.scaleVectorChangeOriginal(2,a);
+        for(int i=0; i<a.length; i++) {
+            assertEquals(exp[i], a[i], 0.0001, " bad value at index " + i);
+        }
+        assertSame(b,a);
+    }
+
+    //@Test
+    void vectorLenght(){
+        double a[] = new double[] {-1.0, 0.0};
+        double r = tobject.vectorLenght(a);
+        assertEquals(1,r);
+    }
+
+
 }
